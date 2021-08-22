c20ae993936a3ef97a00d7780c63adcae7b89d6b
DioSaputraa <diosaputra1414@gmail.com>
2021-08-22 11:29:40 +0700

    menambhakan file index.php

---
+++ index.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+
+<html>
+<head>
+	<meta charset="utf-8">
+	<meta name="viewport" content="width=device-width">
+	<meta http-equiv="X-UA-Compatible" content="ie">
+	<title>coba Git Remote</title>
+</head>
+<body>
+	<h1>Hello world!</h1>
+	<p>Lorem</p>
+</body>
+</html>
